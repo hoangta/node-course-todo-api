672e82cf2f00562dc79cced716dfeb3d4de174dc
Hoang Ta <vinhhoang.ta@2359media.com>
2018-08-20 01:15:24 +0700

    [add] test POST todo.

--- server/server.js
+++ server/server.js
@@ -24,34 +24,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
     console.log('Started on port 3000')
 })
-// var newTodo = new Todo({
-//     text: 'Cook dinner'
-// })
-//
-// newTodo.save().then((doc) => {
-//     console.log('Saved todo', doc)
-// }, (e) => {
-//     console.log(e)
-// })
 
-// var newTodo = new Todo({
-//     text: 'Cook another dinner',
-//     // completed: false,
-//     // completedAt: new Date().getTime()
-// })
-//
-// newTodo.save().then((doc) => {
-//     console.log('Saved todo', doc)
-// }, (e) => {
-//     console.log(e)
-// })
-
-// var newUser = new User({
-//     email: 'phong@example.com'
-// })
-//
-// newUser.save().then((doc) => {
-//     console.log('Saved user', doc)
-// }, (e) => {
-//     console.log(e)
-// })
+module.exports = {app}
---
+++ server/test/server.test.js
@@ -0,0 +1,49 @@
+const request = require('supertest')
+
+const {app} = require('./../server')
+const {Todo} = require('./../models/todo')
+
+beforeEach((finished) => {
+    Todo.remove({}).then(() => finished())
+})
+
+describe('POST /todo', () => {
+    it('should create a new todo', (finished) => {
+        var text = 'Test todo text'
+        request(app)
+            .post('/todos')
+            .send({text})
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.text).toBe(text)
+            })
+            .end((err, res) => {
+                if (err) {
+                    return finished(err)
+                }
+
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(1)
+                    expect(todos[0].text).toBe(text)
+                    finished()
+                }).catch((e) => finished(e))
+            })
+    })
+
+    it('should not create a new todo with invalid data', (finished) => {
+        request(app)
+            .post('/todos')
+            .send({text: ''})
+            .expect(400)
+            .end((err, res) => {
+                if (err) {
+                    return finished(err)
+                }
+
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(0)
+                    finished()
+                }).catch((e) => finished(e))
+            })
+    })
+})
