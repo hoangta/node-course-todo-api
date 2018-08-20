c8cbd4016928315639789fe37d804859e51c9cb7
Hoang Ta <vinhhoang.ta@2359media.com>
2018-08-20 14:19:47 +0700

    [add] test case for GET /todo:id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,11 +1,14 @@
 const request = require('supertest')
+const {ObjectID} = require('mongodb')
 
 const {app} = require('./../server')
 const {Todo} = require('./../models/todo')
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'First test todo'
 }, {
+    _id: new ObjectID(),
     text: 'Second test todo'
 }]
 
@@ -64,6 +67,33 @@ describe('GET /todo', () => {
             .expect((res) => {
                 expect(res.body.todos.length).toBe(2)
             })
+            .end(finished)
+    })
+})
+
+describe('GET /todo:id', () => {
+    it('should return todo', (finished) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                console.log(res.body)
+                expect(res.body.text).toEqual(todos[0].text)
+            })
+            .end(finished)
+    })
+
+    it('should return 404 if todo not found', (finished) => {
+        request(app)
+            .get(`/todos/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(finished)
+    })
+
+    it('should return 404 for non-object ids', (finished) => {
+        request(app)
+            .get('/todos/xyz')
+            .expect(404)
             .end(finished)
     })
 })
