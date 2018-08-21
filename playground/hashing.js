e3c74c2250028daa85e36df64a2e62a90da624f1
Hoang Ta <vinhhoang.ta@2359media.com>
2018-08-21 16:41:21 +0700

    [mod] hash password.

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,33 +1,25 @@
 const {SHA256} = require('crypto-js')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
-var data = {
-    id: 10
-}
-var token = jwt.sign(data, 'pppsecret')
-console.log(token)
+let password = 'pppassword'
 
-let decoded = jwt.verify(token, '2pppsecret')
-console.log('decoded', decoded)
-// var message = 'Hey hey heiii!'
-// var hash = SHA256(message).toString()
-//
-// console.log(`Message: ${message}`)
-// console.log(`Hash: ${hash}`)
-//
+bcrypt.genSalt(13, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash)
+    })
+})
+
+let hashed = '$2a$13$XqoOZ3jMkXjm8uQsWspM0OEvmJidHDmBhnedsZsIjJmaawBEvo/pi'
+bcrypt.compare('sss', hashed, (err, res) => {
+    console.log(res)
+})
+
 // var data = {
-//     id: 4
+//     id: 10
 // }
-// var token = {
-//     data,
-//     hash: SHA256(JSON.stringify(data) + 'secret').toString()
-// }
-// token.data.id = 5
-// token.hash = SHA256(JSON.stringify(data)).toString()
-// var resultHash = SHA256(JSON.stringify(token.data) + 'secret').toString()
-// if (resultHash === token.hash) {
-//     console.log('Can be trusted')
-// }
-// else {
-//     console.log('Can NOT be trusted!')
-// }
+// var token = jwt.sign(data, 'pppsecret')
+// console.log(token)
+//
+// let decoded = jwt.verify(token, '2pppsecret')
+// console.log('decoded', decoded)
